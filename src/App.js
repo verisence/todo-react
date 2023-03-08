a8069d59ba7a01096c4fdd8a404c243be23f7c3b
Phil Kipkemboi <kipkemboiphil@gmail.com>
2023-03-08 18:57:53 +0300

    Add header component and style the app using index.css

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+
+function App() {
+  const name = 'Phil'
+
+  return (
+    <div className="container">
+      <Header title='Task Tracker'></Header>
+    </div>
+  );
+}
+
+export default App;
