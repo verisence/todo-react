38902855985719753ae5b00d3ef5b6e864ad7807
Phil Kipkemboi <kipkemboiphil@gmail.com>
2023-03-20 02:02:04 +0300

    Add delete and toggle reminder functions

--- src/App.js
+++ src/App.js
@@ -14,14 +14,40 @@ const App = () => {
       id: 2,
       text: "Meeting at School",
       day: "Feb 6th at 1:30pm",
-      reminder: true,
+      reminder: false,
     },
   ]);
+
+  // Delete Task
+
+  const deleteTask = (id) => {
+    // console.log(id);
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  // Toggle Reminder
+
+  const toggleReminder = (id) => {
+    // console.log(id);
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
 
   return (
     <div className='container'>
       <Header title='Task Tracker'></Header>
-      <Tasks tasks={tasks}></Tasks>
+      {tasks.length > 0 ? (
+        <Tasks
+          tasks={tasks}
+          onDelete={deleteTask}
+          onToggle={toggleReminder}
+        ></Tasks>
+      ) : (
+        "No Tasks At The Moment"
+      )}
     </div>
   );
 };
